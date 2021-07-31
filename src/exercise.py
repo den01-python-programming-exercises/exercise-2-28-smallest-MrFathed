86bc6da184f191b9eac1aa5643bfb3093de7b7d7
Justin Turner <jmturner638@gmail.com>
2021-07-31 00:17:09 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #write your code below this line
+    pass
+
+def smallest(number1, number2):
+    """
+    Inputs two numbers
+    Returns the smallest of the two
+    """
+    if number1 < number2:
+        return number1
+    return number2
 
 if __name__ == '__main__':
     main()
